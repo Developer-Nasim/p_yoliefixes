02b6fd5adfbd5b2bdb277c42208031c13b393fa8
Developer-Nasim <ajnasim72@gmail.coom>
2024-02-23 15:28:45 +0600

    fixed header, img borders, replaced img etc

--- custom.js
+++ custom.js
@@ -1,5 +1,26 @@
 window.addEventListener('load', () => {
+
+    function HeaderMenuResponsive() {
+        const mBtns = document.querySelectorAll(".showMenuForMobile")
+        const mItems = document.querySelectorAll(".headMemu")
+        mBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                document.body.classList.toggle('show_menu')
+            })
+        })
+    }
+    HeaderMenuResponsive()
+
+
+
+
+
+
+
     
+
+
+
     function HeaderMoreExpend() {
         const moreBtns = document.querySelectorAll('.more_items')
         const moreBlk = document.querySelectorAll('.all_more_items')
@@ -13,4 +34,6 @@ window.addEventListener('load', () => {
         })
     }
     HeaderMoreExpend()
+
+    
 })
